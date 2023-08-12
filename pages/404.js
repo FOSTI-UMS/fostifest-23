0800a5d6c485f01db802e1f2c116e1b6fbb649e9
W. SYAFIUDDIN <encrypt0r-x@protonmail.com>
2023-08-12 21:15:09 +0700

    404 pages

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+export default function Custom404() {
+    return (
+        <div className="container my-5">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="text-center">404</h1>
+                    <h2 className="text-center">Not Found</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
