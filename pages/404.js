32d6f27083c79987c7a213667da46ee50e74ff8c
W. SYAFIUDDIN <encrypt0r-x@protonmail.com>
2023-08-18 20:04:43 +0700

    fix login-regist

--- pages/404.js
+++ pages/404.js
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Custom404() {
     return (
-        <div class="d-flex align-items-center justify-content-center vh-100">
-            <div class="text-center">
-                <h1 class="display-1 fw-bold">404</h1>
-                <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
-                <p class="lead">
+        <div className="d-flex align-items-center justify-content-center vh-100">
+            <div className="text-center">
+                <h1 className="display-1 fw-bold">404</h1>
+                <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
+                <p className="lead">
                     The page you're looking for doesn't exist.
                     </p>
-                <Link href="/" class="btn btn-primary">Go Home</Link>
+                <Link href="/" className="btn btn-primary">Go Home</Link>
             </div>
         </div>
     );
