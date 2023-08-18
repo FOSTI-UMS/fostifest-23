e11b15fa039f5aa396dbf447107da7e178c11402
Fnrzz <faridnurrgantenng123@gmail.com>
2023-08-18 14:24:59 +0700

    [PUSH] Revisi Login & Register Page

--- components/elements/login/index.js
+++ components/elements/login/index.js
@@ -6,60 +6,69 @@ import Lottie from "lottie-react";
 export default function FormLogin() {
   return (
     <div className="container">
-      <div
-        className="row row-cols-md-2 row-cols-1 align-items-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="col d-flex justify-content-center">
-          <Lottie animationData={LoginGif} className="w-75" />
+      <div className="row align-items-center" style={{ minHeight: "100vh" }}>
+        <div className="col-md-6 d-none d-md-block">
+          <div className="d-flex justify-content-center" data-aos="fade">
+            <Lottie animationData={LoginGif} style={{ width: "60%" }} />
+          </div>
         </div>
-        <div className="col">
-          <div className={`text-center container ${styles["form-box"]}`}>
-            <div className="fw-bold pt-5">Login</div>
-            <div className="d-flex align-items-center justify-content-center flex-column mb-3 ">
-              <label
-                htmlFor="inputEmail5"
-                className={`fw-bold form-label me-auto ms-4 ${styles["label-custom"]}`}
+        <div className="col-md-6">
+          <div
+            className={`card rounded-4 shadow ${styles["card-width"]}`}
+            data-aos="fade"
+          >
+            <div className="card-body">
+              <div className="fw-bold pt-5 text-center">Login</div>
+              <form>
+                <div className="d-flex align-items-center justify-content-center flex-column mb-3 ">
+                  <label
+                    htmlFor="inputEmail5"
+                    className={`fw-bold form-label me-auto ms-4 ${styles["label-custom"]}`}
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="inputEmail5"
+                    name="email"
+                    className={`form-control ${styles["input-custom"]}`}
+                    aria-describedby="emailHelpBlock"
+                  />
+                </div>
+                <div className="d-flex align-items-center justify-content-center flex-column mb-5">
+                  <label
+                    htmlFor="inputPassword5"
+                    className={`fw-bold form-label me-auto ms-4 ${styles["label-custom"]}`}
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="inputPassword5"
+                    name="password"
+                    className={`form-control ${styles["input-custom"]}`}
+                    aria-describedby="passwordHelpBlock"
+                  />
+                </div>
+                <div className="text-white fw-bold text-decoration-none d-flex align-items-center justify-content-center">
+                  <button
+                    type="submit"
+                    className={`mb-3 text-white fw-bold d-flex align-items-center justify-content-center ${styles["btn-login"]}`}
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+
+              <Link
+                href="/register"
+                className="text-dark fw-bold text-decoration-none mb-5 d-flex align-items-center justify-content-center"
               >
-                Email
-              </label>
-              <input
-                type="email"
-                id="inputEmail5"
-                className={`form-control ${styles["input-custom"]}`}
-                aria-describedby="emailHelpBlock"
-              />
+                <div className={`${styles["change-page"]}`}>
+                  Belum punya akun?
+                </div>
+              </Link>
             </div>
-            <div className="d-flex align-items-center justify-content-center flex-column mb-5">
-              <label
-                htmlFor="inputPassword5"
-                className={`fw-bold form-label me-auto ms-4 ${styles["label-custom"]}`}
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                id="inputPassword5"
-                className={`form-control ${styles["input-custom"]}`}
-                aria-describedby="passwordHelpBlock"
-              />
-            </div>
-            <div className="text-white fw-bold text-decoration-none d-flex align-items-center justify-content-center">
-              <button
-                type="submit"
-                className={`mb-3 text-white fw-bold d-flex align-items-center justify-content-center ${styles["btn-login"]}`}
-              >
-                Login
-              </button>
-            </div>
-            <Link
-              href="/register"
-              className="text-dark fw-bold text-decoration-none mb-5 d-flex align-items-center justify-content-center"
-            >
-              <div className={`${styles["change-page"]}`}>
-                Belum punya akun?
-              </div>
-            </Link>
           </div>
         </div>
       </div>
--- components/elements/register/index.js
+++ components/elements/register/index.js
@@ -1,104 +1,150 @@
 import styles from "./register.module.css";
 import Link from "next/link";
+import { useState } from "react";
+import LoginGif from "/assets/gifs/login.json";
+import Lottie from "lottie-react";
 
 export default function FormRegister() {
+  const [page, setPage] = useState("first");
+
   return (
-    <div className="container py-3">
-      <div
-        className="row align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="col-md-8">
-          <div className="card rounded-4 shadow">
+    <div className="container">
+      <div className="row align-items-center" style={{ minHeight: "100vh" }}>
+        <div className="col-md-6 d-none d-md-block">
+          <div className="d-flex justify-content-center" data-aos="fade">
+            <Lottie animationData={LoginGif} style={{ width: "60%" }} />
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div
+            className={`card rounded-4 shadow ${styles["card-width"]}`}
+            data-aos="fade"
+          >
             <div className="card-body">
-              <div className="fw-bold my-2  text-center">Register</div>
+              <div className="fw-bold my-2 text-center">Register</div>
               <form>
-                <div className="mb-2">
-                  <label
-                    htmlFor="namaLengkap"
-                    className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
-                  >
-                    Nama Lengkap
-                  </label>
-                  <input
-                    type="text"
-                    className={`form-control ${styles["input-custom"]}`}
-                  />
+                <div className={page === "first" ? "d-block" : "d-none"}>
+                  <div className="mb-2">
+                    <label
+                      htmlFor="namaLengkap"
+                      className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
+                    >
+                      Nama Lengkap
+                    </label>
+                    <input
+                      type="text"
+                      id="namaLengkap"
+                      name="namaLengkap"
+                      className={`form-control ${styles["input-custom"]}`}
+                    />
+                  </div>
+                  <div className="mb-2">
+                    <label
+                      htmlFor="alamat"
+                      className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
+                    >
+                      Alamat
+                    </label>
+                    <input
+                      type="text"
+                      id="alamat"
+                      name="alamat"
+                      className={`form-control ${styles["input-custom"]}`}
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="noTelp"
+                      className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
+                    >
+                      No Telp
+                    </label>
+                    <input
+                      type="number"
+                      id="noTelp"
+                      name="noTelp"
+                      className={`form-control ${styles["input-custom"]}`}
+                    />
+                  </div>
+                  <div className="d-grid">
+                    <button
+                      type="button"
+                      onClick={() => setPage("second")}
+                      className={`btn ${styles["btn-login"]} rounded-4`}
+                    >
+                      Next
+                    </button>
+                  </div>
                 </div>
-                <div className="mb-2 ">
-                  <label
-                    htmlFor="alamat"
-                    className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
-                  >
-                    Alamat
-                  </label>
-                  <input
-                    type="text"
-                    className={`form-control ${styles["input-custom"]}`}
-                  />
-                </div>
-                <div className="mb-2 ">
-                  <label
-                    htmlFor="noTelp"
-                    className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
-                  >
-                    No Telp
-                  </label>
-                  <input
-                    type="number"
-                    className={`form-control ${styles["input-custom"]}`}
-                  />
-                </div>
-                <div className="mb-2 ">
-                  <label
-                    htmlFor="inputEmail5"
-                    className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    className={`form-control ${styles["input-custom"]}`}
-                    aria-describedby="emailHelpBlock"
-                  />
-                </div>
-                <div className="mb-2">
-                  <label
-                    htmlFor="inputPassword5"
-                    className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    className={`form-control ${styles["input-custom"]}`}
-                    aria-describedby="passwordHelpBlock"
-                  />
-                </div>
-                <div className="mb-2">
-                  <label
-                    htmlFor="inputChoose"
-                    className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
-                  >
-                    Pilih Event
-                  </label>
-                  <select className={`form-select ${styles["input-custom"]}`}>
-                    <option value={"webinar"}>Webinar</option>
-                    <option value={"lomba"}>Lomba Landing Page</option>
-                  </select>
-                </div>
-                <div className="text-white fw-bold text-decoration-none d-flex align-items-center justify-content-center">
-                  <button
-                    type="submit"
-                    className={`my-3 text-white fw-bold d-flex align-items-center justify-content-center ${styles["btn-login"]}`}
-                  >
-                    Register
-                  </button>
+                <div className={page === "second" ? "d-block" : "d-none"}>
+                  <div className="mb-2">
+                    <a
+                      onClick={() => setPage("first")}
+                      className={`${styles["btn-back"]} text-decoration-none ms-2`}
+                    >
+                      Kembali
+                    </a>
+                  </div>
+                  <div className="mb-2">
+                    <label
+                      htmlFor="inputEmail5"
+                      className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="inputEmail5"
+                      name="email"
+                      className={`form-control ${styles["input-custom"]}`}
+                      aria-describedby="emailHelpBlock"
+                    />
+                  </div>
+                  <div className="mb-2">
+                    <label
+                      htmlFor="inputPassword5"
+                      className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="inputPassword5"
+                      name="password"
+                      className={`form-control ${styles["input-custom"]}`}
+                      aria-describedby="passwordHelpBlock"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="inputChoose"
+                      className={`fw-bold form-label me-auto ms-2 ${styles["label-custom"]}`}
+                    >
+                      Pilih Event
+                    </label>
+                    <select
+                      id="inputChoose"
+                      name="event"
+                      className={`form-select ${styles["input-custom"]}`}
+                    >
+                      <option value={"webinar"}>Webinar</option>
+                      <option value={"lomba"}>Lomba Landing Page</option>
+                    </select>
+                  </div>
+                  <div className="d-grid">
+                    <button
+                      type="submit"
+                      className={`btn ${styles["btn-login"]} rounded-4`}
+                    >
+                      Register
+                    </button>
+                  </div>
                 </div>
               </form>
+
               <Link
                 href="/login"
-                className="text-dark fw-bold text-decoration-none mb-5 d-flex align-items-center justify-content-center"
+                className="text-dark fw-bold text-decoration-none mb-5 d-flex align-items-center justify-content-center mt-3"
               >
                 <div className={`${styles["change-page"]}`}>
                   Sudah punya akun?
