5429c28174b64319c6ab901ac6f1bd75b708ca55
Fnrzz <faridnurrgantenng123@gmail.com>
2023-08-18 21:00:46 +0700

    [FIXED] layout mobile fileCollection page

--- components/elements/afterRegister/fileCollection.js
+++ components/elements/afterRegister/fileCollection.js
@@ -1,12 +1,16 @@
 import styles from "./style.module.css";
-import { FiLogOut } from 'react-icons/fi';
+import { FiLogOut } from "react-icons/fi";
 
 export default function FileCollection() {
   return (
-    <div className={`${styles["content-pengumpulan"]} `}>
-       <button className={`${styles["logout-btn"]}`}>
-      <FiLogOut /> Logout
-    </button>
+    <div className={`${styles["content-pengumpulan"]} `} data-aos="fade">
+      <div className="row mb-3">
+        <div className="col d-flex justify-content-end">
+          <button className={`${styles["logout-btn"]} btn rounded-4`}>
+            <FiLogOut /> Logout
+          </button>
+        </div>
+      </div>
       <div
         className={`fw-bold text-center mb-5 d-flex justify-content-center align-items-center  ${styles["title"]}`}
       >
