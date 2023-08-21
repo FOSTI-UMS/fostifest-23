79d6a8fa13fd6c1d7d936f1ca435ca3d03bb82fe
Fnrzz <faridnurrgantenng123@gmail.com>
2023-08-21 20:44:50 +0700

    [PUSH] add modal verif payment in get-data-lomba

--- components/elements/admin/get-data-lomba.js
+++ components/elements/admin/get-data-lomba.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import styles from "./admin.module.css";
 import supabase from "@/api/supabase";
+import Lottie from "lottie-react";
+import paymentsImage from "@/assets/gifs/payments.json";
 
 export default function GetDataLomba() {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
   const [search, setSearch] = useState(users);
+  const [verif, setVerif] = useState("");
 
   const fetchUsers = async () => {
     const { data: usersData, error: fetchError } = await supabase
@@ -31,7 +34,7 @@ export default function GetDataLomba() {
   };
 
   return (
-    <div className="container pt-3 mt-3">
+    <div className="container pb-3">
       <p className="fw-bold">Data User Lomba</p>
       <div className={`mb-3 ${styles["search-box"]}`}>
         <input
@@ -44,58 +47,131 @@ export default function GetDataLomba() {
         <p className="text-danger">Maaf, Tidak/Belum Ada Data Peserta Lomba</p>
       )}
       {search.length > 0 && (
-        <table className={`table table-striped ${styles["table"]}`}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Nama</th>
-              <th>Alamat</th>
-              <th>Instansi</th>
-              <th>Email</th>
-              <th>Jenis</th>
-              <th>No Telp</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            {search.map((user) => (
-              <tr
-                key={user.id_user}
-                style={{
-                  fontSize: "14px",
-                }}
-              >
-                <td data="ID">{user.id_user}</td>
-                <td data="Nama">{user.nama}</td>
-                <td data="Alamat">{user.alamat}</td>
-                <td data="Instansi">{user.instansi}</td>
-                <td data="Email">{user.email}</td>
-                <td data="Jenis">{user.jenis}</td>
-                <td data="No Telp">{user.no_telp}</td>
-                <td data="Aksi">
-                  <div className="d-flex gap-3">
-                    <button
-                      className="btn btn-outline-success btn-sm"
-                      onClick={() => {
-                        window.location.href = `/api/verif/${user.nama}`;
-                      }}
-                    >
-                      Verifikasi Pembayaran
-                    </button>
-                    <button
-                      className="btn btn-outline-danger btn-sm"
-                      onClick={() => {
-                        window.location.href = `/api/hapus/${user.nama}`;
-                      }}
-                    >
-                      Hapus User
-                    </button>
-                  </div>
-                </td>
+        <div>
+          <table
+            className={`table table-responsive table-striped ${styles["table"]}`}
+            style={{ whiteSpace: "nowrap" }}
+          >
+            <thead>
+              <tr>
+                <th>NO</th>
+                <th>Nama</th>
+                <th>Alamat</th>
+                <th>Instansi</th>
+                <th>Email</th>
+                <th>Jenis</th>
+                <th>No Telp</th>
+                <th>Status</th>
+                <th>Aksi</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {search.map((user, index) => (
+                <tr
+                  key={index}
+                  style={{
+                    fontSize: "14px",
+                  }}
+                >
+                  <td data="NO">{index + 1}</td>
+                  <td data="Nama">{user.nama}</td>
+                  <td data="Alamat">{user.alamat}</td>
+                  <td data="Instansi">{user.instansi}</td>
+                  <td data="Email">{user.email}</td>
+                  <td data="Jenis">{user.jenis}</td>
+                  <td data="No Telp">{user.no_telp}</td>
+                  <td data="Status">
+                    {user.payment_verif
+                      ? "Terverifikasi"
+                      : "Belum Terverifikasi"}
+                  </td>
+                  <td data="Aksi">
+                    <div className="d-flex gap-3">
+                      {!user.payment_verif && (
+                        <button
+                          type="button"
+                          className="btn btn-outline-success btn-sm"
+                          data-bs-toggle="modal"
+                          data-bs-target={`#modal-payments`}
+                          onClick={() => setVerif(user.nama)}
+                        >
+                          Verifikasi Pembayaran
+                        </button>
+                      )}
+                      <button
+                        className="btn btn-outline-danger btn-sm"
+                        onClick={() => {
+                          window.location.href = `/api/hapus/${user.nama}`;
+                        }}
+                      >
+                        Hapus User
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div
+            className="modal fade"
+            id={`modal-payments`}
+            tabIndex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h1 className="modal-title fs-5" id="exampleModalLabel">
+                    Verifikasi Pembayaran
+                  </h1>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  {verif && (
+                    <div className="container-fluid">
+                      <div className="d-flex justify-content-center">
+                        <Lottie
+                          animationData={paymentsImage}
+                          autoPlay={true}
+                          loop={true}
+                          className="w-50"
+                        />
+                      </div>
+                      <div className="my-4 text-center">
+                        <small className="fw-bold">
+                          Verfikasi Pembayaran Atas Nama {verif} ?
+                        </small>
+                      </div>
+                      <div className="d-flex justify-content-center">
+                        <button
+                          className="btn btn-success rounded-4"
+                          onClick={() => {
+                            window.location.href = `/api/verif/${verif}`;
+                          }}
+                        >
+                          Confirm
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-danger ms-3 rounded-4"
+                          data-bs-dismiss="modal"
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
