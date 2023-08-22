bff7a12083ebbf1b8c341dbb92a406681c72a094
Fnrzz <faridnurrgantenng123@gmail.com>
2023-08-22 21:15:41 +0700

    [PUSH] add profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,13 +1,9 @@
-import { useRouter } from 'next/router';
-import Logout from '@/components/elements/logout';
-import Profile from '@/components/elements/user/profile';
+import Profile from "@/components/elements/user/profile";
 
 export default function HomeProfile() {
-
-    return (
-        <div className='mt-5 pt-5'>
-            <Profile />
-            <Logout />
-        </div>
-    );
+  return (
+    <div className="mt-5 pt-5">
+      <Profile />
+    </div>
+  );
 }
