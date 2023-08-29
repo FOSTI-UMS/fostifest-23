0d38eec9cb93d88b2b1f7d06d2d049780629c624
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-29 18:51:44 +0700

    [UPDATE] view login & register page

--- components/elements/login/index.js
+++ components/elements/login/index.js
@@ -1,12 +1,13 @@
 import styles from "./login.module.css";
 import Link from "next/link";
-import LoginGif from "/assets/gifs/login.json";
+import LoginGif from "/assets/gifs/login_2.json";
 import SuccesImage from "@/assets/gifs/succesfully.json";
 import Lottie from "lottie-react";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import supabase from "@/api/supabase";
 import Cookies from "js-cookie";
+import BackgroundSide from "@/assets/images/bg_login.png";
 
 export default function FormLogin() {
   const dibuka = new Date("2023-08-25T00:00:00+07:00");
@@ -16,9 +17,7 @@ export default function FormLogin() {
     return (
       <div className="d-flex align-items-center justify-content-center vh-100">
         <div className="text-center">
-          <p className="lead">
-            Halaman ini akan dibuka pada tanggal 30 Agustus 2021.
-          </p>
+          <p className="lead">Halaman ini akan dibuka pada tanggal 30 Agustus 2021.</p>
           <Link href="/" className="btn btn-sm btn-primary">
             Go Home
           </Link>
@@ -69,79 +68,51 @@ export default function FormLogin() {
   }, []);
 
   return (
-    <div className="container">
-      <div className="row align-items-center" style={{ minHeight: "100vh" }}>
-        <div className="col-md-6 d-none d-md-block">
-          <div className="d-flex justify-content-center" data-aos="fade">
-            <Lottie animationData={LoginGif} style={{ width: "60%" }} />
+    <div className="container d-flex justify-content-center align-items-center p-0" style={{ minHeight: "100vh" }} 
+    data-aos="fade"
+    >
+      <div className="row card container mx-3 d-flex flex-row shadow border border-none p-0" style={{ minHeight: "80vh" }}>
+        <div
+          className="col-md-6 d-none d-lg-block border-end rounded-start-2"
+          style={{ 
+            backgroundImage: `url(${BackgroundSide.src})`,
+            backgroundSize: "cover",
+            height: "100%",
+          }}
+        >
+          <div
+            className="d-flex justify-content-center"
+          >
+            <Lottie animationData={LoginGif} style={{ width: "75%", minHeight: "80vh" }} />
           </div>
         </div>
-        <div className="col-md-6">
+        <div className="col-lg-6">
           <div
-            className={`card rounded-4 shadow ${styles["card-width"]}`}
-            data-aos="fade"
+            className={`${styles["card-width"]}`}
           >
             <div className="card-body">
-              <div className="fw-bold pt-5 text-center">Login</div>
-              <form onSubmit={handleLogin}>
+              <div className="my-5 fs-2 text-center">LOGIN</div>
+              <form onSubmit={handleLogin} >
                 <div className="d-flex align-items-center justify-content-center flex-column mb-3 ">
-                  <label
-                    htmlFor="inputEmail5"
-                    className={`fw-bold form-label me-auto ms-4 ${styles["label-custom"]}`}
-                  >
+                  <label htmlFor="inputEmail5" className={`fw-bold form-label me-auto ms-4 ${styles["label-custom"]}`}>
                     Email
                   </label>
-                  <input
-                    type="email"
-                    id="inputEmail5"
-                    name="email"
-                    className={`form-control ${styles["input-custom"]}`}
-                    aria-describedby="emailHelpBlock"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
+                  <input type="email" id="inputEmail5" name="email" className={`form-control ${styles["input-custom"]}`} aria-describedby="emailHelpBlock" onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="d-flex align-items-center justify-content-center flex-column mb-5">
-                  <label
-                    htmlFor="inputPassword5"
-                    className={`fw-bold form-label me-auto ms-4 ${styles["label-custom"]}`}
-                  >
+                  <label htmlFor="inputPassword5" className={`fw-bold form-label me-auto ms-4 ${styles["label-custom"]}`}>
                     Password
                   </label>
-                  <input
-                    type="password"
-                    id="inputPassword5"
-                    name="password"
-                    className={`form-control ${styles["input-custom"]}`}
-                    aria-describedby="passwordHelpBlock"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
+                  <input type="password" id="inputPassword5" name="password" className={`form-control ${styles["input-custom"]}`} aria-describedby="passwordHelpBlock" onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div className="text-white fw-bold text-decoration-none d-flex align-items-center justify-content-center">
-                  <button
-                    type="submit"
-                    className={`mb-3 text-white fw-bold d-flex align-items-center justify-content-center ${styles["btn-login"]}`}
-                    onClick={() => setLoading(true)}
-                  >
-                    {loading ? (
-                      <span
-                        className="spinner-border spinner-border-sm"
-                        role="status"
-                        aria-hidden="true"
-                      ></span>
-                    ) : (
-                      "Login"
-                    )}
+                  <button type="submit" className={`mb-3 rounded-5 text-white d-flex align-items-center justify-content-center ${styles["btn-login"]}`} onClick={() => setLoading(true)}>
+                    {loading ? <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>: <span>Login</span>}
                   </button>
                 </div>
               </form>
-
-              <Link
-                href="/register"
-                className="text-dark fw-bold text-decoration-none mb-5 d-flex align-items-center justify-content-center"
-              >
-                <div className={`${styles["change-page"]}`}>
-                  Belum punya akun?
-                </div>
+              <Link href="/register" className="text-dark fw-bold text-decoration-none mb-5 d-flex align-items-center justify-content-center">
+                <div className={`${styles["change-page"]}`}>Belum punya akun?</div>
               </Link>
             </div>
           </div>
@@ -149,30 +120,17 @@ export default function FormLogin() {
       </div>
       {showModal && (
         <div>
-          <div
-            className="modal fade show d-block"
-            tabIndex="-1"
-            role="dialog"
-            aria-hidden="true"
-            onClick={closeModal}
-          >
+          <div className="modal fade show d-block" tabIndex="-1" role="dialog" aria-hidden="true" onClick={closeModal}>
             <div className="modal-dialog modal-dialog-centered">
               <div className="modal-content">
                 <div className="modal-body">
                   <div className="container-fluid">
                     <div className="d-flex justify-content-center">
-                      <Lottie
-                        animationData={SuccesImage}
-                        autoPlay={true}
-                        loop={true}
-                        className="w-75"
-                      />
+                      <Lottie animationData={SuccesImage} autoPlay={true} loop={true} className="w-75" />
                     </div>
                     <div className="mb-4 text-center">
                       <h3 className="fw-bold mb-3">Yeayy Selamat !</h3>
-                      <small className="text-secondary">
-                        Anda Telah Berhasil Mendaftar Silahkan Check Email Anda
-                      </small>
+                      <small className="text-secondary">Anda Telah Berhasil Mendaftar Silahkan Check Email Anda</small>
                     </div>
                   </div>
                 </div>
