81b1641fa73d29f1a83a1be1479c364876937d10
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-08-31 17:25:46 +0700

    [ADD] end page

---
+++ pages/end/index.js
@@ -0,0 +1,10 @@
+import PendaftaranTutup from "@/components/elements/afterRegister/tutup";
+
+
+export default function EndPage() {
+  return (
+    <div>
+      <PendaftaranTutup />
+    </div>
+  );
+}
