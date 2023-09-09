9275bfd652ba69cde152ccc9386c66cd120b37e7
Fnrzz <faridnurrgantenng123@gmail.com>
2023-09-09 11:49:27 +0700

    [PUSH] add Media Partner

--- components/elements/home/mediaPartner/index.js
+++ components/elements/home/mediaPartner/index.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import UmsLogo from "@/assets/images/ums-logo.png";
 import RapmaFmLogo from "@/assets/images/rapma-logo.png";
+import FinicLogo from "@/assets/images/finic-logo.PNG";
 import HimakomLogo from "@/assets/images/himakom-logo.jpg";
+import InformatikaLogo from "@/assets/images/informatika-logo.png";
 
 export default function MediaPartner() {
   return (
@@ -11,25 +13,20 @@ export default function MediaPartner() {
       data-aos="fade-up"
     >
       <h3 className="fw-bold mb-5">Media Partner</h3>
-      <div className="row row-cols-3 row-cols-md-4 align-items-start justify-content-center">
+      <div className="row row-cols-3 row-cols-md-4 align-items-center justify-content-center g-4">
         <div className="col d-flex justify-content-center">
-          <Image
-            src={RapmaFmLogo}
-            alt="rapma"
-            className="w-75 h-50"
-          />
+          <Image src={FinicLogo} alt="finic" className="w-75 h-50" />
         </div>
         <div className="col d-flex justify-content-center ">
-          <Image
-            src={UmsLogo}
-            alt="ums"
-            className="w-75 h-50"
-          />
+          <Image src={UmsLogo} alt="ums" className="w-75 h-50" />
+        </div>
+        <div className="col d-flex justify-content-center">
+          <Image src={HimakomLogo} alt="himakom" className="w-75 h-50" />
         </div>
         <div className="col d-flex justify-content-center">
           <Image
-            src={HimakomLogo}
-            alt="himakom"
+            src={InformatikaLogo}
+            alt="informatika"
             className="w-75 h-50"
           />
         </div>
