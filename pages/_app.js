1f838bcc605075992f8e8dee27c6d6655234665d
Fnrzz <faridnurrgantenng123@gmail.com>
2023-09-13 14:06:46 +0700

    [PUSH] edit Head meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,20 @@ function MyApp({ Component, pageProps }) {
   return (
     <AppShell>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>FOSTIFEST</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content={`FOSTIFEST (FOSTI Festival) merupakan kegiatan tahunan yang diselenggarakan oleh Forum Open Source Teknik Informatika (FOSTI) UMS. FOSTIFEST 2023 merupakan ajang yang keempat dengan mengusung tema "The Art of Proficiency and Landing Page Allure".`}
+        />
+        <meta property="og:title" content="FOSTIFEST" />
+        <meta
+          property="og:description"
+          content={`FOSTIFEST (FOSTI Festival) merupakan kegiatan tahunan yang diselenggarakan oleh Forum Open Source Teknik Informatika (FOSTI) UMS. FOSTIFEST 2023 merupakan ajang yang keempat dengan mengusung tema "The Art of Proficiency and Landing Page Allure".`}
+        />
+        <meta property="og:url" content="https://fostifest.fostiums.org" />
+        <meta property="og:image" content="/logo_fostifest_meta_tag.png" />
+        <meta property="og:type" content="website" />
       </Head>
       <Component {...pageProps} />
       <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" />
