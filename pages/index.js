5bac837633ff4474e76af170a09671bef96e7232
Fnrzz <faridnurrgantenng123@gmail.com>
2023-09-13 14:02:53 +0700

    [PUSH] add posterEvent

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ import MediaPartner from "@/components/elements/home/mediaPartner";
 import SocialMedia from "@/components/elements/home/socialMedia";
 import FostiFest from "@/components/elements/home/fostiFest";
 import RoadShow from "@/components/elements/home/roadShow";
-import TimelineImg from "@/assets/images/timeline.png";
-import Image from "next/image";
+import PosterEvent from "@/components/elements/home/posterEvent";
 import Head from "next/head";
 
 export default function Index() {
@@ -15,11 +14,17 @@ export default function Index() {
     <div className="pb-5">
       <Head>
         <title>FOSTIFEST</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta name="description" content={`FOSTIFEST (FOSTI Festival) merupakan kegiatan tahunan yang diselenggarakan oleh Forum Open Source Teknik Informatika (FOSTI) UMS. FOSTIFEST 2023 merupakan ajang yang keempat dengan mengusung tema "The Art of Proficiency and Landing Page Allure".`} />
-          <meta property="og:title" content="FOSTIFEST" />
-          <meta property="og:description" content={`FOSTIFEST (FOSTI Festival) merupakan kegiatan tahunan yang diselenggarakan oleh Forum Open Source Teknik Informatika (FOSTI) UMS. FOSTIFEST 2023 merupakan ajang yang keempat dengan mengusung tema "The Art of Proficiency and Landing Page Allure".`} />
-          <meta property="og:url" content="https://fostifest.fostiums.org" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content={`FOSTIFEST (FOSTI Festival) merupakan kegiatan tahunan yang diselenggarakan oleh Forum Open Source Teknik Informatika (FOSTI) UMS. FOSTIFEST 2023 merupakan ajang yang keempat dengan mengusung tema "The Art of Proficiency and Landing Page Allure".`}
+        />
+        <meta property="og:title" content="FOSTIFEST" />
+        <meta
+          property="og:description"
+          content={`FOSTIFEST (FOSTI Festival) merupakan kegiatan tahunan yang diselenggarakan oleh Forum Open Source Teknik Informatika (FOSTI) UMS. FOSTIFEST 2023 merupakan ajang yang keempat dengan mengusung tema "The Art of Proficiency and Landing Page Allure".`}
+        />
+        <meta property="og:url" content="https://fostifest.fostiums.org" />
         <meta property="og:image" content="/logo_fostifest_meta_tag.png" />
         <meta property="og:type" content="website" />
       </Head>
@@ -28,7 +33,7 @@ export default function Index() {
       <RoadShow />
       <EventContent />
       <CompetitionEvent />
-      <Image className="d-flex justify-content-center align-items-center m-auto my-5 timeline-img" src={TimelineImg} alt="timeline"/>
+      <PosterEvent />
       <SupportedBy />
       <MediaPartner />
       <SocialMedia />
