4ff4ec3a96709b1bb0c7bc934ea06fce5ae0a4c0
W. SYAFIUDDIN <encrypt0r-x@protonmail.com>
2023-09-19 20:38:58 +0700

    updt xlsx

--- components/elements/admin/dwnld.js
+++ components/elements/admin/dwnld.js
@@ -1,18 +1,26 @@
 import supabase from "@/pages/api/supabase";
 import * as XLSX from 'xlsx';
 
-// get data user then save it as xlsx
 export default async function getDataUser() {
-    const { data: user, error } = await supabase
+    const noww = new Date().getTime();
+    const { data: user_lomba } = await supabase
         .from("users")
         .select("id_user, nama, email, jenis, no_telp")
-        .neq('jenis', 'PANITIA')
+        .eq('jenis', 'LOMBA DESIGN')
+        .eq('is_admin', false)
+        .eq('super_admin', false)
+        .order('nama');
+    const { data: user_webinar } = await supabase
+        .from("users")
+        .select("id_user, nama, email, jenis, no_telp")
+        .eq('jenis', 'WEBINAR')
         .eq('is_admin', false)
         .eq('super_admin', false)
         .order('nama');
 
-    const wb = XLSX.utils.book_new();
-    const ws = XLSX.utils.json_to_sheet(user);
+    const book = XLSX.utils.book_new();
+    const sheetLomba = XLSX.utils.json_to_sheet(user_lomba);
+    const sheetWebinar = XLSX.utils.json_to_sheet(user_webinar);
 
     const columnWidths = [
         { wpx: 72 },
@@ -21,7 +29,9 @@ export default async function getDataUser() {
         { wpx: 140 },
         { wpx: 140 },
     ];
-    ws['!cols'] = columnWidths;
-    XLSX.utils.book_append_sheet(wb, ws, "data semua users");
-    XLSX.writeFile(wb, "public/data-user.xlsx");
+    sheetLomba['!cols'] = columnWidths;
+    sheetWebinar['!cols'] = columnWidths;
+    XLSX.utils.book_append_sheet(book, sheetLomba, "LOMBA");
+    XLSX.utils.book_append_sheet(book, sheetWebinar, "WEBINAR");
+    XLSX.writeFile(book, `data_user_${noww}.xlsx`);
 }
