028ceabf230beb0b8d577380098cf6c4f86320a4
W. SYAFIUDDIN <encrypt0r-x@protonmail.com>
2023-10-26 07:37:43 +0700

    add stats

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,4 +1,5 @@
 import download from "@/components/elements/admin/dwnld";
+import Statistics from "@/components/elements/admin/stat";
 
 export default function Home() {
   return (
@@ -6,6 +7,8 @@ export default function Home() {
       <div className="row align-items-center h-100">
         <div className="d-flex flex-column align-items-center justify-content-center">
           <h3 className="fw-bold text-center">Selamat Datang Admin</h3>
+          <p>Statistik:</p>
+          <Statistics />
           <button className="btn btn-outline-primary text-center" onClick={download}>
             Download Data User
           </button>
